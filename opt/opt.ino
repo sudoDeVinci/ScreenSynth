529b4f65a8b1fb3493d3175563007c4dfe0a71bb
tadj <tadj.d.cazaubon@gmail.com>
2024-07-01 19:17:24 -0700

    changes to generating sine wave

--- opt/opt.ino
+++ opt/opt.ino
@@ -1,22 +1,65 @@
-#include "sound.h"
-#include <driver/adc.h>
-#include <Arduino.h>
+#include <driver/i2s.h>
+#include <math.h>
+
+#define PIN_BCLK 42
+#define PIN_LRCK 2
+#define PIN_DIN 41
+
+const int sampleRate = 44100;
+const int bitsPerSample = 16;
+const int num_channels = 1;
+const int bufferSize = 1024;
+
+const int16_t constant = (1 << 15) - 1;
+const float frequency_C = 261.63;
+
+int16_t* buffer = new int16_t[bufferSize];
+
+void generate_sine_wave(float voltage, float frequency, int sample_rate = sampleRate, int16_t* audioBuf = buffer) {
+  //int16_t* buffer = new int16_t[bufferSize];
+  float index = voltage / 3.3;
+  int num_samples = (int)(sample_rate / frequency);
+  float phase_increment = 2 * PI * frequency;
+  float x = phase_increment / sample_rate;
+
+  for (int i = 0; i < num_samples; i++) {
+    int amplitude = (int)(constant * sin(x*i) * index);
+    audioBuf[i] = (int16_t)(amplitude * sin(i * x));
+  }
+}
 
 void setup() {
-    Serial.begin(115200);
-    init_i2s();
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Setting up.");
 
-    // Configure ADC
-    analogReadResolution(12); // 12-bit resolution
-    analogSetAttenuation(ADC_11db); // 11dB attenuation for full-scale voltage range
+  i2s_config_t i2s_config = {
+    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
+    .sample_rate = sampleRate,
+    .bits_per_sample = (i2s_bits_per_sample_t)bitsPerSample,
+    .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
+    .communication_format = I2S_COMM_FORMAT_I2S_MSB,
+    .intr_alloc_flags = 0,
+    .dma_buf_count = 8,
+    .dma_buf_len = bufferSize,
+    .use_apll = false,
+    .tx_desc_auto_clear = true,
+    .fixed_mclk = 0
+  };
+
+  i2s_pin_config_t pin_config = {
+    .bck_io_num = PIN_BCLK,
+    .ws_io_num = PIN_LRCK,
+    .data_out_num = PIN_DIN,
+    .data_in_num = I2S_PIN_NO_CHANGE
+  };
+
+  i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+  i2s_set_pin(I2S_NUM_0, &pin_config);
+  generate_sine_wave(1.2, frequency_C, sampleRate);
 }
 
 void loop() {
-    int adc_value = analogRead(36); // Read from GPIO36 (ADC1_CHANNEL_0)
-    float voltage = (3.3f / 4095) * adc_value;
-
-    SineWaveGenerator generator(voltage, 261.63f);
-    send_sine_wave(generator);
-
-    delay(5); // Small delay to avoid overwhelming the I2S interface
+  size_t bytes_written;
+  i2s_write(I2S_NUM_0, buffer, bufferSize * sizeof(int16_t), &bytes_written, portMAX_DELAY);
 }
--- opt/sound.h
+++ opt/sound.h
@@ -1,10 +1,4 @@
 #ifndef SOUND_H
 #define SOUND_H
 
-#include <driver/i2s.h>
-#include "sine.h"
-
-void init_i2s();
-void send_sine_wave(SineWaveGenerator& generator);
-
 #endif // SOUND_H
