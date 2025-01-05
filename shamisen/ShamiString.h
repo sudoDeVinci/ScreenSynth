4750394be1c4f8c8a1ff6650b82ba3a05d9b3c89
tadj cazaubon <tadj.d.cazaubon@gmail.com>
2025-01-05 10:19:00 +0100

    no more lasers - and now a smaller monitor that uses RCA

--- shamisen/ShamiString.h
+++ shamisen/ShamiString.h
@@ -1,20 +1,20 @@
 #include <Arduino.h>
-#define ADC_MAX 4095;
+#define ADC_MAX 4095
 
-struct Ito {
+struct ShamiString {
   uint16_t LOW_NOTE;
   uint16_t HIGH_NOTE;
   uint8_t PIN;
   int THRESH = 50;
 
-  Ito() {
-      ;
+  ShamiString () {
+    ;
   }
 
-  Ito (uint16_t low_note,
-      uint16_t high_note,
-      uint8_t pin
-    ) {
+  ShamiString (uint16_t low_note,
+               uint16_t high_note,
+               uint8_t pin
+              ) {
     this -> LOW_NOTE = low_note;
     this -> HIGH_NOTE = high_note;
     this -> PIN = pin;
@@ -27,7 +27,9 @@ struct Ito {
     return s;
   }
 
-  uint32_t getFreq () {
-
+  uint32_t freq () {
+    uint16_t val = this -> read();
+    int freq = map(val, 0, ADC_MAX, this->LOW_NOTE, this->HIGH_NOTE);
+    return freq;
   }
 };
--- shamisen/shamisen.ino
+++ shamisen/shamisen.ino
@@ -1,4 +1,5 @@
 #include "pitches.h"
+#include "ShamiString.h"
 // 12 bit precision for the output frequency
 #define RESOLUTION  12
 
@@ -9,23 +10,27 @@
 // String pins - 4,5 & 6
 #define STRING_PIN_ICHI 4
 #define STRING_PIN_NI 5
-#define STRING_PIN_SAN 6
-
 
 /*
  * Use 8000 Hz as the base output frequency
  * This acts as the volume control for the speaker.
+ * We may wanna change these later with dials.
  */
-uint32_t volume = 8000;
+uint32_t volume_left = 8000;
+uint32_t volume_right = 8000;
 
 // Queues to hold frequencies 
-QueueHandle_t ichi_Queue;
-QueueHandle_t ni_Queue;
-QueueHandle_t san_Queue;
+QueueHandle_t ichiQueue;
+QueueHandle_t niQueue;
 
 // Producer and Consumer tasks.
-void readStrings(void * parameter);
-void playAudio(void * parameter);
+void readStrings(void * parameters);
+void playAudio(void * parameters);
+
+// Our Strings
+ShamiString ichi = ShamiString(NOTE_C2, NOTE_C5, STRING_PIN_ICHI);
+ShamiString ni = ShamiString(NOTE_F2, NOTE_F5, STRING_PIN_NI);
+
 
 void setup() {
     Serial.begin(115200);
@@ -34,9 +39,8 @@ void setup() {
     analogReadResolution(RESOLUTION);
     
     // Create Queues to hold frequency values
-    ichi_Queue = xQueueCreate(10, sizeof(uint32_t));
-    ni_Queue = xQueueCreate(10, sizeof(uint32_t));
-    san_Queue = xQueueCreate(10, sizeof(uint32_t));
+    ichiQueue = xQueueCreate(10, sizeof(uint32_t));
+    niQueue = xQueueCreate(10, sizeof(uint32_t));
     
     // Create tasks
     xTaskCreate(
@@ -56,4 +60,49 @@ void setup() {
         1,                // Priority of the task
         NULL             // Task handle
     );      
+}
+
+/**
+ * Read and enqueue the current frequencies from our strings.
+ */
+void readStrings(void * parameters) {
+  while (true) {
+    uint32_t ichiFreq = ichi.freq();
+    uint32_t niFreq = ni.freq();
+
+    xQueueSend(ichiQueue, &ichiFreq, portMAX_DELAY);
+    xQueueSend(niQueue, &niFreq, portMAX_DELAY);
+
+    delay(10);
+  }
+}
+
+void playAudio(void * parameters) {
+  ledcAttachChannel(AUDIO_PIN_LEFT, volume_left, RESOLUTION, 1);
+  ledcAttachChannel(AUDIO_PIN_RIGHT, volume_right, RESOLUTION, 1);
+  bool attached = true;
+
+  // Frequency buffers
+  uint32_t ichiFreq;
+  uint32_t niFreq;
+
+  // Frequency buffers
+  uint32_t lastIchiFreq = 0;
+  uint32_t lastNiFreq = 0;
+
+  while(true) {
+    // Update last known frequency 
+    if (xQueueReceive(ichiQueue, &ichiFreq, 0) == pdPASS) lastIchiFreq = ichiFreq;
+    if (xQueueReceive(niQueue, &niFreq, 0) == pdPASS) lastNiFreq = niFreq;
+
+    ledcWriteTone(AUDIO_PIN_LEFT, lastIchiFreq);
+    ledcWriteTone(AUDIO_PIN_RIGHT, lastNiFreq);
+    delay(5);
+  }
+}
+
+void loop () {
+  while (true) {
+    delay(2000000000);
+  }
 }
