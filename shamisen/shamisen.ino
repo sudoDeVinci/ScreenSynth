6e8a39a3fcb8b2e7a4ce747426c42d7952878447
Tadj cazaubon <tadj.d.cazaubon@gmail.com>
2025-01-13 19:31:56 +0100

    strings good - havent implemented strum key and volume value yet

--- shamisen/shamisen.ino
+++ shamisen/shamisen.ino
@@ -1,102 +1,155 @@
 #include "pitches.h"
-#include "ShamiString.h"
+#include "softpot.h"
+
+/**
+ * This program acts as a synthesizer, allowing the user to play tones from the ESP32S3 as you would a Shamisen.
+ * This is inspired by the CRT instruments created by the band Electronicos Fantasticos - specifically the Telesen.
+ * The "strings" are expected to be softpots, though technically any potentiometer can work.
+ * 
+ * Tones and volume are read into a queue for each string in one process, and are outputted as pwm signals
+ * on pins pinned to individual pwm chanels. While this doesn't give the best audio quality, we only need a 
+ * consistent tone output. 
+ */
+
 // 12 bit precision for the output frequency
-#define RESOLUTION  12
+#define RESOLUTION 12
 
 // Speaker pins - left and right.
-#define AUDIO_PIN_LEFT 47
-#define AUDIO_PIN_RIGHT 15
+#define AUDIO_PIN_LEFT 2
+#define AUDIO_PIN_MIDDLE 42
+#define AUDIO_PIN_RIGHT 41
 
 // String pins - 4,5 & 6
 #define STRING_PIN_ICHI 4
 #define STRING_PIN_NI 5
+#define STRING_PIN_SAN 6
+#define VOLUME_PIN 7
 
-/*
- * Use 8000 Hz as the base output frequency
- * This acts as the volume control for the speaker.
- * We may wanna change these later with dials.
- */
-uint32_t volume_left = 8000;
-uint32_t volume_right = 8000;
+// Volume value changed with softpot slider.
+uint16_t volume = 8000;
 
-// Queues to hold frequencies 
+// Queues to hold frequencies.
 QueueHandle_t ichiQueue;
 QueueHandle_t niQueue;
-
-// Producer and Consumer tasks.
-void readStrings(void * parameters);
-void playAudio(void * parameters);
-
-// Our Strings
-ShamiString ichi = ShamiString(NOTE_C2, NOTE_C5, STRING_PIN_ICHI);
-ShamiString ni = ShamiString(NOTE_F2, NOTE_F5, STRING_PIN_NI);
-
-
-void setup() {
-    Serial.begin(115200);
-
-    // Configure ADC resolution
-    analogReadResolution(RESOLUTION);
-    
-    // Create Queues to hold frequency values
-    ichiQueue = xQueueCreate(10, sizeof(uint32_t));
-    niQueue = xQueueCreate(10, sizeof(uint32_t));
-    
-    // Create tasks
-    xTaskCreate(
-        readStrings,   // Function to implement the task
-        "ReadStringsTask", // Name of the task
-        10000,            // Stack size in words
-        NULL,             // Task input parameter
-        1,                // Priority of the task
-        NULL             // Task handle);
-    );
-    
-    xTaskCreate(
-        playAudio,    // Function to implement the task
-        "PlayAudioTask",  // Name of the task
-        10000,            // Stack size in words
-        NULL,             // Task input parameter
-        1,                // Priority of the task
-        NULL             // Task handle
-    );      
-}
+QueueHandle_t sanQueue;
+QueueHandle_t volumeQueue;
 
 /**
- * Read and enqueue the current frequencies from our strings.
+ * Producer task for audio pipeline.
+ * Read in the audio from the strings, as well as our volume into cross-process queues.
+ * @param parameters: Nothing - We don't actually pass anything in here.
  */
+void readStrings(void * parameters);
+
+/**
+ * Consumer task for audio pipeline.
+ * Attach audio pins to pwm outputs, and play sounds from the queue w/ given volume.\
+ * @param parameters: Nothing - We don't actually pass anything in here.
+ */
+void playAudio(void * parameters);
+
+// Our Strings and volume slider.
+SoftPot ichi = SoftPot(NOTE_C2, NOTE_C5, STRING_PIN_ICHI);
+SoftPot ni = SoftPot(NOTE_F2, NOTE_F5, STRING_PIN_NI);
+SoftPot san = SoftPot(NOTE_F2, NOTE_F5, STRING_PIN_SAN);
+SoftPot volumeSlider = SoftPot(0, volume, VOLUME_PIN, 0);
+
+void setup() {
+  Serial.begin(115200);
+
+  // Configure ADC resolution
+  analogReadResolution(RESOLUTION);
+  
+  // Create Queues to hold frequency values
+  ichiQueue = xQueueCreate(10, sizeof(uint16_t));
+  niQueue = xQueueCreate(10, sizeof(uint16_t));
+  sanQueue = xQueueCreate(10, sizeof(uint16_t));
+  volumeQueue = xQueueCreate(10, sizeof(uint16_t));
+  
+  // Create tasks
+  xTaskCreate(
+      readStrings,   // Function to implement the task
+      "ReadStringsTask", // Name of the task
+      10000,            // Stack size in words
+      NULL,             // Task input parameter
+      1,                // Priority of the task
+      NULL             // Task handle);
+  );
+  
+  xTaskCreate(
+      playAudio,    // Function to implement the task
+      "PlayAudioTask",  // Name of the task
+      10000,            // Stack size in words
+      NULL,             // Task input parameter
+      1,                // Priority of the task
+      NULL             // Task handle
+  );      
+}
+
+
 void readStrings(void * parameters) {
   while (true) {
-    uint32_t ichiFreq = ichi.freq();
-    uint32_t niFreq = ni.freq();
+    uint16_t ichiFreq = ichi.freq();
+    uint16_t niFreq = ni.freq();
+    uint16_t sanFreq = san.freq();
+    uint16_t volumeFreq = volumeSlider.freq(); 
+
 
     xQueueSend(ichiQueue, &ichiFreq, portMAX_DELAY);
     xQueueSend(niQueue, &niFreq, portMAX_DELAY);
+    xQueueSend(sanQueue, &sanFreq, portMAX_DELAY);
+    xQueueSend(volumeQueue, &volumeFreq, portMAX_DELAY);
 
     delay(10);
   }
 }
 
 void playAudio(void * parameters) {
-  ledcAttachChannel(AUDIO_PIN_LEFT, volume_left, RESOLUTION, 1);
-  ledcAttachChannel(AUDIO_PIN_RIGHT, volume_right, RESOLUTION, 1);
+  /*
+   * We attach a dedicated pwm channel to each "string", outputting to our audio pins.
+   * There are 8 channels, 0-7. It's good to have some space between channels used - 
+   * - in my experience, so we take every 3rd channel.
+   */
+  ledcAttachChannel(AUDIO_PIN_LEFT, volume, RESOLUTION, 0);
+  ledcAttachChannel(AUDIO_PIN_MIDDLE, volume, RESOLUTION, 3);
+  ledcAttachChannel(AUDIO_PIN_RIGHT, volume, RESOLUTION, 6);
   bool attached = true;
 
   // Frequency buffers
-  uint32_t ichiFreq;
-  uint32_t niFreq;
+  uint16_t ichiFreq;
+  uint16_t niFreq;
+  uint16_t sanFreq;
+  uint16_t volumeFeq;
 
-  // Frequency buffers
-  uint32_t lastIchiFreq = 0;
-  uint32_t lastNiFreq = 0;
+  /*
+   * Technically we don't exactly need these - but they give us slightly smoother audio.
+   * These are the last successful sound made by the strings (the queue returns 'pdPASS').
+   *
+   * We fetch the sounds in bulk, and play them one after the other.
+   * Fetching then playing each within their own if statement makes the audio hitch slightly. 
+   */
+  uint16_t lastIchiFreq = 0;
+  uint16_t lastNiFreq = 0;
+  uint16_t lastSanFreq = 0;
+  uint16_t lastVolumeFreq = 0;
+
+  /**
+  * We use this to tell that a string is being touched.
+  */
+  uint8_t threshold = 50;
 
   while(true) {
     // Update last known frequency 
     if (xQueueReceive(ichiQueue, &ichiFreq, 0) == pdPASS) lastIchiFreq = ichiFreq;
     if (xQueueReceive(niQueue, &niFreq, 0) == pdPASS) lastNiFreq = niFreq;
+    if (xQueueReceive(sanQueue, &sanFreq, 0) == pdPASS) lastSanFreq = sanFreq;
 
     ledcWriteTone(AUDIO_PIN_LEFT, lastIchiFreq);
-    ledcWriteTone(AUDIO_PIN_RIGHT, lastNiFreq);
+    ledcWriteTone(AUDIO_PIN_MIDDLE, lastNiFreq);
+    ledcWriteTone(AUDIO_PIN_RIGHT, lastSanFreq);
+
+    Serial.println(lastSanFreq);
+
     delay(5);
   }
 }
--- shamisen/softpot.h
+++ shamisen/softpot.h
@@ -1,35 +1,37 @@
 #include <Arduino.h>
 #define ADC_MAX 4095
 
-struct ShamiString {
+struct SoftPot {
   uint16_t LOW_NOTE;
   uint16_t HIGH_NOTE;
   uint8_t PIN;
-  int THRESH = 50;
+  uint8_t THRESH;
 
-  ShamiString () {
+  SoftPot () {
     ;
   }
 
-  ShamiString (uint16_t low_note,
-               uint16_t high_note,
-               uint8_t pin
-              ) {
+  SoftPot (uint16_t low_note,
+           uint16_t high_note,
+           uint8_t pin,
+           uint8_t thresh=10
+          ) {
     this -> LOW_NOTE = low_note;
     this -> HIGH_NOTE = high_note;
     this -> PIN = pin;
+    this -> THRESH = thresh;
   }
 
   uint16_t read () {
     uint16_t s = analogRead(this -> PIN);
     if (s >= ADC_MAX) s = ADC_MAX;
-    if (s <  0 ) s = ADC_MAX;
+    if (s <  0 ) s = this -> THRESH;
     return s;
   }
 
-  uint32_t freq () {
+  uint16_t freq () {
     uint16_t val = this -> read();
-    int freq = map(val, 0, ADC_MAX, this->LOW_NOTE, this->HIGH_NOTE);
+    uint16_t freq = map(val, this->THRESH, ADC_MAX, this->LOW_NOTE, this->HIGH_NOTE);
     return freq;
   }
 };
